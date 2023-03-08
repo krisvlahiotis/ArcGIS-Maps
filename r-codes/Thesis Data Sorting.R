82d91af84556d34af821b7e2f22133c12d78389c
krisvlahiotis <124912575+krisvlahiotis@users.noreply.github.com>
2023-03-08 10:25:28 -0500

    Added in data filters. Need to troubleshoot errors.

--- r-codes/Thesis Data Sorting.R
+++ r-codes/Thesis Data Sorting.R
@@ -14,7 +14,76 @@ auk::auk_set_ebd_path("C:/Users/kv8/Documents/Brock Biology/BIOL 4F91/GIS Data/e
 install.packages("auk")
 library(auk)
 
+#Bird Conservation Region - Lower Great Lakes
+auk_bcr(bcr = 13)
+  
+
 #Filtering the data
-auk_protocol(protocol = c("Stationary","Traveling")) %>%
+ebd_filters <- ebd %>% 
+auk_species("Black-capped Chickadee") %>% 
+# lower great lakes bcr
+auk_bcr(bcr = 13) %>% 
+# january 1993 to january 2023
+auk_date(date = c("1993-01-01", "2023-01-01")) %>% 
+# restrict to the standard traveling and stationary count protocols
+auk_protocol(protocol = c("Stationary", "Traveling")) %>% 
 auk_complete()
-auk_filter()
+
+#GIS Layers
+library(sf)
+library(rnaturalearth)
+library(dplyr)
+
+# file to save spatial data
+gpkg_dir <- "data"
+if (!dir.exists(gpkg_dir)) {
+  dir.create(gpkg_dir)
+}
+f_ne <- file.path(gpkg_dir, "gis-data.gpkg")
+
+# download bcrs
+tmp_dir <- normalizePath(tempdir())
+tmp_bcr <- file.path(tmp_dir, "bcr.zip")
+paste0("https://www.birdscanada.org/research/gislab/download/", 
+       "bcr_terrestrial_shape.zip") %>% 
+  download.file(destfile = tmp_bcr)
+unzip(tmp_bcr, exdir = tmp_dir)
+bcr <- file.path(tmp_dir, "BCR_Terrestrial_master_International.shp") %>% 
+  read_sf() %>% 
+  select(bcr_code = BCR, bcr_name = LABEL) %>% 
+  filter(bcr_code == 27)
+# clean up
+list.files(tmp_dir, "bcr", ignore.case = TRUE, full.names = TRUE) %>% 
+  unlink()
+
+# political boundaries
+# land border with lakes removed
+ne_land <- ne_download(scale = 50, category = "cultural",
+                       type = "admin_0_countries_lakes",
+                       returnclass = "sf") %>%
+  filter(CONTINENT == "North America") %>%
+  st_set_precision(1e6) %>%
+  st_union()
+# country lines
+# downloaded globally then filtered to north america with st_intersect()
+ne_country_lines <- ne_download(scale = 50, category = "cultural",
+                                type = "admin_0_boundary_lines_land",
+                                returnclass = "sf") %>% 
+  st_geometry()
+ne_country_lines <- st_intersects(ne_country_lines, ne_land, sparse = FALSE) %>%
+  as.logical() %>%
+  {ne_country_lines[.]}
+# states, north america
+ne_state_lines <- ne_download(scale = 50, category = "cultural",
+                              type = "admin_1_states_provinces_lines",
+                              returnclass = "sf") %>%
+  filter(adm0_a3 %in% c("USA", "CAN")) %>%
+  mutate(iso_a2 = recode(adm0_a3, USA = "US", CAN = "CAN")) %>% 
+  select(country = adm0_name, country_code = iso_a2)
+
+# output
+unlink(f_ne)
+write_sf(ne_land, f_ne, "ne_land")
+write_sf(ne_country_lines, f_ne, "ne_country_lines")
+write_sf(ne_state_lines, f_ne, "ne_state_lines")
+write_sf(bcr, f_ne, "bcr")
